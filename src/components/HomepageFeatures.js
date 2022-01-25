15c2801c6a79470455c55c70a1d0ce8e7fedd0b8
Ryan <mr.rmcook@gmail.com>
2022-01-25 14:10:06 +0200

    Fix apostrophe

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,43 +1,47 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
 
 const FeatureList = [
   {
-    title: 'Software Engineer',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    title: "Software Engineer",
+    Svg: require("../../static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        I'm a software engineer with {new Date().getFullYear() - 2015} year's of experience. My primary skillset includes C# and React, 
-        with most of my time being spent on enterprise backend services.
+        I'm a software engineer with {new Date().getFullYear() - 2015} years of
+        experience. My primary skillset includes C# and React, with most of my
+        time being spent on enterprise backend services.
       </>
     ),
   },
   {
-    title: 'Paragliding Pilot',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Paragliding Pilot",
+    Svg: require("../../static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        My free time is spent travelling and paragliding at the most beautiful locations I can find. 
-        I have been paragliding for {new Date().getFullYear() - 2020} year and enjoy flying long distance and following the sport.
+        My free time is spent travelling and paragliding at the most beautiful
+        locations I can find. I have been paragliding for{" "}
+        {new Date().getFullYear() - 2020} year and enjoy flying long distance
+        and following the sport.
       </>
     ),
   },
   {
-    title: 'About me',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    title: "About me",
+    Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        I live in South Africa and have since my birth date. I am currently {new Date().getFullYear() - 1993} years old.
-        I have been overseas on more than one occasion, but would love to do it many more times!
+        I live in South Africa and have since my birth date. I am currently{" "}
+        {new Date().getFullYear() - 1993} years old. I have been overseas on
+        more than one occasion, but would love to do it many more times!
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div>
